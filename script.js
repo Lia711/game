a36915778ad93a4b5f423c115b9896a2d136f428
Liana Petrova <lianapetrova00@gmail.com>
2023-04-11 14:47:47 +0100

    1st question loads

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 import { questionArray } from "./questions.js"
+
 const contentContainer=document.querySelector(".content-container")
 const contentContainer2=document.querySelector(".content-container-2")
-let questionBox;
-let answersBox;
-contentContainer2.style.display="none";
 const startButton=document.querySelector(".start")
+const questionBox=document.querySelector(".question");
+const answersBox=document.querySelectorAll(".answer");
 
+//on load question box is hidden
+contentContainer2.style.display="none";
+
+//load questions; hide start page
 const switchPage = () => {
     contentContainer.style.display="none";
     contentContainer2.style.display="block";
-    questionBox=document.querySelector(".question");
-    answersBox=document.querySelectorAll(".answer");
     renderQuestion();
     renderAnswers();
 }
 
+//if start button is displayed, look for click
 if (startButton!==null) {
     startButton.addEventListener("click", switchPage)
 }
@@ -23,11 +26,10 @@ const renderQuestion = () => {
     questionBox.innerHTML=questionArray[0].text
 }
 
-
 const renderAnswers = () => {
-    answersBox[0].innerHTML=questionArray.answer1;
-    answersBox[1].innerHTML=questionArray.answer2;
-    answersBox[2].innerHTML=questionArray.answer3;
-    answersBox[3].innerHTML=questionArray.answer4;
+    answersBox[0].innerHTML=questionArray[0].answer1;
+    answersBox[1].innerHTML=questionArray[0].answer2;
+    answersBox[2].innerHTML=questionArray[0].answer3;
+    answersBox[3].innerHTML=questionArray[0].answer4;
 }
 
