dd392ed38faade84a62fb676c32e454b39d187e2
Liana Petrova <lianapetrova00@gmail.com>
2023-04-12 14:32:24 +0100

    implemented money counter

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const questionBox=document.querySelector(".question");
 const answersBox=document.querySelectorAll(".answer");
 const lifelineButton=document.querySelectorAll(".lifeline-button");
 const nextButton=document.querySelector(".next")
+const moneyDisplay=document.querySelector(".money")
 
 let questionCount = 0
 
@@ -39,8 +40,9 @@ const renderAnswers = () => {
 
 const handleAnswer = (event) => {
     const answer = event.target
-    if (answer.innerHTML==questionArray[0].correct) {
+    if (answer.innerHTML==questionArray[questionCount].correct) {
         answer.style.backgroundColor="green";
+        countMoney();
     } else {
         answer.style.backgroundColor="red";
     }
@@ -52,6 +54,7 @@ answersBox.forEach((button)=> {
 
 const handleSkip = () => {
     questionCount++;
+    lifelineButton[2].style.backgroundColor="gray";
     renderQuestion();
     renderAnswers();
 }
@@ -65,4 +68,61 @@ const handleNext = () => {
 }
 
 nextButton.addEventListener("click", handleNext)
+
+const countMoney = () => {
+    let moneyAdded=0
+    switch(questionCount) {
+        case 0:
+            moneyAdded+=100;
+            break;
+        case 1:
+            moneyAdded+=200;
+            break;
+        case 2:
+            moneyAdded+=300;
+            break;
+        case 3:
+            moneyAdded+=500;
+            break;
+        case 4:
+            moneyAdded+=1000;
+        case 5:
+            moneyAdded+=2000;
+            break;
+        case 6:
+            moneyAdded+=4000;
+            break;
+        case 7:
+            moneyAdded+=8000;
+            break;
+        case 8:
+            moneyAdded+=16000;
+            break;
+        case 9:
+            moneyAdded+=32000;
+            break;
+        case 10:
+            moneyAdded+=64000;
+            break;
+        case 11:
+            moneyAdded+=125000;
+            break;
+        case 12:
+            moneyAdded+=250000;
+            break;
+        case 13: 
+            moneyAdded+=500000;
+            break;
+        case 14:
+            moneyAdded+=1000000;
+            break;
+    }
+    moneyDisplay.innerText=`Money Acquired: £${moneyAdded}`
+}
+
+//fix styling
+//fix answer colors
+//make endgame
+//implement other lifelines
+
 
