2398176af07c911c162cc8acc907a5f2d25be06d
Liana Petrova <lianapetrova00@gmail.com>
2023-04-12 15:49:10 +0100

    added questions

--- questions.js
+++ questions.js
@@ -1,123 +1,123 @@
 export const questionArray = [
   {
-    text: "Q1",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A2"
+    text: "Q1: What color are the hottest stars?",
+    answer1: "Red",
+    answer2: "Blue",
+    answer3: "Orange",
+    answer4: "Yellow",
+    correct: "Blue"
   },
   {
-    text: "Q2",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A1"
+    text: "Q2: What type of galaxy is our Milky Way?",
+    answer1: "An elliptical galaxy",
+    answer2: "A spiral galaxy",
+    answer3: "An irregular galaxy",
+    answer4: "A round galaxy",
+    correct: "A spiral galaxy"
   },
   {
-    text: "Q3",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A2"
+    text: "Q3: What is the ultimate fate of our Sun?",
+    answer1: "White dwarf",
+    answer2: "Red giant",
+    answer3: "Black hole",
+    answer4: "Supernova",
+    correct: "White dwarf"
   },
   {
-    text: "Q4",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A3"
+    text: "Q4: One astronomical unit (AU) is equal to",
+    answer1: "The mean distance of Earth from the sun",
+    answer2: "The circumference of Earth's orbit around the sun",
+    answer3: "The diameter of the sun",
+    answer4: "The time required for Earth to complete one revolution",
+    correct: "The mean distance of Earth from the sun"
   },
   {
-    text: "Q5",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A2"
+    text: "Q5: Who first proposed that the universe is expanding?",
+    answer1: "Albert Einstein",
+    answer2: "Edwin Hubble",
+    answer3: "Johannes Kepler",
+    answer4: "James Clerk Maxwell",
+    correct: "Edwin Hubble"
   },
   {
-    text: "Q6",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A4"
+    text: "Q6: Which of the four fundamental forces found in nature is the weakest?",
+    answer1: "Gravity",
+    answer2: "Electromagnetic",
+    answer3: "Strong nuclear",
+    answer4: "Weak nuclear",
+    correct: "Gravity"
   },
   {
-    text: "Q7",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A4"
+    text: "Q7: Black dwarf stars have likely never been observed because:",
+    answer1: "They are made of dark matter",
+    answer2: "They would take longer to form than the universe has existed",
+    answer3: "They are too far away",
+    answer4: "They all died out after the big bang",
+    correct:"They would take longer to form than the universe has existed"
   },
   {
-    text: "Q8",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A1"
-  },
-      {
-    text: "Q9",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct:"A3"
+    text: "Q8: Which of the following does NOT influence the gravitational acceleration acting on a planet?",
+    answer1: "The mass of the star it orbits",
+    answer2: "The gravitational constant, G",
+    answer3: "The mass of the planet itself",
+    answer4: "The distance from the star it orbits",
+    correct: "The mass of the planet itself"
   },
   {
-    text: "Q10",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A1"
+    text: "Q9: As a rule, more massive stars:",
+    answer1: "Are always hotter than smaller stars",
+    answer2: "Have a shorter lifespan than smaller stars",
+    answer3: "Are always more luminous than smaller stars",
+    answer4: "Have a longer lifespan than smaller stars",
+    correct: "Have a shorter lifespan than smaller stars"
   },
   {
-    text: "Q11",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A2"
+    text: "Q10: What is the name of a rotating neutron star that emits a rotating beam of electromagnetic radiation?",
+    answer1: "Quasar",
+    answer2: "Reptar",
+    answer3: "Protostar",
+    answer4: "Pulsar",
+    correct: "Pulsar"
   },
   {
-    text: "Q12",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A4"
+    text: "Q11: Transition from a high energy to a low energy state in an atom or molecule results in:",
+    answer1: "Red shift",
+    answer2: "Blue shift",
+    answer3: "Absorption of a photon at a particular wavelength",
+    answer4: "Emission of a photon at a particular wavelength",
+    correct: "Emission of a photon at a particular wavelength"
   },
   {
-    text: "Q13",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct:"A2"
+    text: "Q12: The core of a red supergiant star consists of:",
+    answer1: "Iron",
+    answer2: "Hydrogen",
+    answer3: "Carbon",
+    answer4: "Helium",
+    correct: "Iron"
   },
   {
-    text: "Q14",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A2"
+    text: "Q13: The electromagnetic radiation with the lowest known frequency is:",
+    answer1: "Gamma rays",
+    answer2: "Ultraviolet rays",
+    answer3: "Radio waves",
+    answer4: "X-rays",
+    correct:"Radio waves"
   },
   {
-    text: "Q15",
-    answer1: "A1",
-    answer2: "A2",
-    answer3: "A3",
-    answer4: "A4",
-    correct: "A3"
+    text: "Q14: A red giant occurs when:",
+    answer1: "Nucleosynthesis has stopped",
+    answer2: "Hydrogen is fused into helium",
+    answer3: "Hydrogen has been depleted at the star's core",
+    answer4: "Elements heavier than carbon have been synthesized",
+    correct: "Hydrogen has been depleted at the star's core"
+  },
+  {
+    text: "Q15: In Young's double slit experiment, the brightest lines observed were caused by:",
+    answer1: "Interference of waves out of phase",
+    answer2: "Interference of waves in phase",
+    answer3: "The light reaching the area first",
+    answer4: "The area containing specific elements",
+    correct: "Interference of waves in phase"
   }];
 
   export default questionArray;
--- script.js
+++ script.js
@@ -76,53 +76,55 @@ const countMoney = () => {
             moneyAdded+=100;
             break;
         case 1:
-            moneyAdded+=200;
+            moneyAdded+=100;
             break;
         case 2:
-            moneyAdded+=300;
+            moneyAdded+=100;
             break;
         case 3:
-            moneyAdded+=500;
+            moneyAdded+=200;
             break;
         case 4:
-            moneyAdded+=1000;
+            moneyAdded+=500;
         case 5:
-            moneyAdded+=2000;
+            moneyAdded+=1000;
             break;
         case 6:
-            moneyAdded+=4000;
+            moneyAdded+=2000;
             break;
         case 7:
-            moneyAdded+=8000;
+            moneyAdded+=4000;
             break;
         case 8:
-            moneyAdded+=16000;
+            moneyAdded+=8000;
             break;
         case 9:
-            moneyAdded+=32000;
+            moneyAdded+=16000;
             break;
         case 10:
-            moneyAdded+=64000;
+            moneyAdded+=32000;
             break;
         case 11:
-            moneyAdded+=125000;
+            moneyAdded+=61000;
             break;
         case 12:
-            moneyAdded+=250000;
+            moneyAdded+=125000;
             break;
         case 13: 
-            moneyAdded+=500000;
+            moneyAdded+=250000;
             break;
         case 14:
-            moneyAdded+=1000000;
+            moneyAdded+=500000;
             break;
     }
     moneyDisplay.innerText=`Money Acquired: £${moneyAdded}`
 }
 
-//fix styling
+//fix styling for small screen width
 //fix answer colors
 //make endgame
 //implement other lifelines
+//cannot click more than one button per question
+//fix money calculations
 
 
